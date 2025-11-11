dc695efd58dd38abc1918ef7253d2faa9f683a40
Iuri d'Oliveira de Lima <doliveiraiuri@gmail.com>
2025-11-11 17:14:28 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+
+  const [email, setEmail] = useState('mail')
+  const [senha, setSenha] = useState('')
   return (
     <View style={styles.container}>
+      <Text>Usuário: {email}</Text>
+      <TextInput placeholder='Digite seu email'></TextInput>
+      <Text>Senha</Text>
+      <TextInput placeholder='Digite sua senha'></TextInput>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Entrar'></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +25,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    // DOUGLAS É UM BOSTINHA
   },
 });
